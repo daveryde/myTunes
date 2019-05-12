dc6f0e0fc1a79e773c27701e49972f1a7ed8d051
daveryde <d.mcordova87@gmail.com>
2019-05-11 19:58:37 -0700

    code format

--- js/main.js
+++ js/main.js
@@ -29,9 +29,7 @@ $(function() {
 $('.cf a').on('click', function(event) {
   if (this.hash !== '') {
     event.preventDefault();
-
     const hash = this.hash;
-
     $('html, body').animate(
       {
         scrollTop: $(hash).offset().top
